f9ed6ba706faaf921b0b40f4d6a67b76474b2063
johnhunsley <snowball33>
2017-07-10 16:58:15 +0100

    refactor for auth0

---
+++ src/components/Callback.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="spinner">
+    <img src="../assets/loading.svg" alt="loading"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'callback',
+  props: ['auth'],
+  data () {
+    this.auth.handleAuthentication()
+    this.$router.push('/client')
+    return {}
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,25 @@ import Mint from 'mint-ui'
 import Login from '@/components/Login'
 import Client from '@/components/Client'
 import EventsList from '@/components/EventsList'
+import EventsPagedList from '@/components/EventsPagedList'
 import Event from '@/components/Event'
 import Map from '@/components/Map'
-import auth from '../components/auth.js'
+import Callback from '@/components/Callback'
+import AuthService from '../components/AuthService'
 
 Vue.use(VueResource)
 Vue.use(Router)
 Vue.use(Mint)
 
+const auth = new AuthService()
+
 function requireAuth (to, from, next) {
-  if (!auth.loggedIn()) {
-    console.log('not logged in, forwarding to /login')
-    next({
-      path: '/login'
-    })
+  console.log('requires auth')
+
+  if (!auth.isAuthenticated()) {
+    alert('not logged in, forwarding to /login')
+    auth.login()
+    next()
   } else {
     console.log('logged in, forwarding to next()')
     next()
@@ -30,8 +35,10 @@ export default new Router({
   routes: [
     {path: '/', redirect: '/login'},
     {path: '/login', component: Login},
+    {path: '/callback', component: Callback, props: {auth}},
     {path: '/client', component: Client, beforeEnter: requireAuth},
     {path: '/events', component: EventsList, beforeEnter: requireAuth},
+    {path: '/eventspage', component: EventsPagedList, beforeEnter: requireAuth},
     {path: '/map', component: Map, beforeEnter: requireAuth},
     {path: '/map/:eventId', component: Map, beforeEnter: requireAuth, props: true},
     {path: '/event/:eventId', component: Event, beforeEnter: requireAuth, props: true}
