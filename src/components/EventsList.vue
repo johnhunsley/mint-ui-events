16af4dcadc6353c6a86a19befbd3b3c32c4610a0
johnhunsley <snowball33>
2017-09-04 15:06:43 +0100

    link to client view from event list

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <mt-header title="Service Provider View">
-      <router-link to="/" slot="left">
-        <mt-button icon="back">back</mt-button>
+      <router-link to="/client" slot="left">
+        <mt-button icon="back">Client</mt-button>
       </router-link>
       <mt-button @click.native="handleLogout()" slot="right">logout</mt-button>
     </mt-header><br/>
